0398371378abd9282aaf67a4649385d5c19334b1
Skrapgon <ISolkinI@yandex.ru>
2025-04-13 15:46:44 +0300

    first commit

---
+++ src/main/java/com/logistic/logistic_dss/LogisticDssApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.logistic_dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticDssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticDssApplication.class, args);
+	}
+
+}
